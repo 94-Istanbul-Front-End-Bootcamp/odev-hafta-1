5528e8b375f7378ff9a17723e3cc90f40bf0d534
SevtapErtem <81636466+SevtapErtem@users.noreply.github.com>
2021-06-24 23:26:31 +0300

    Ödev yapıldı.

---
+++ index.js
@@ -0,0 +1,18 @@
+function namesurname() {
+    let name = "Sevtap Ertem"
+    let age = "28"
+
+    document.getElementById("nameSurname").innerHTML= name;
+    document.getElementById("age").innerHTML = age;
+
+    document.getElementById("button").style.display="none";
+    
+}
+
+function linkedin(){
+    window.open ('https://www.linkedin.com/in/sevtap-ertem-756542148/');
+}
+
+function github(){
+    window.open ('https://github.com/SevtapErtem');
+}
